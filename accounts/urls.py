aa06536bad470120ce64fe6e8f65e4145653cfd5
Sarah Mazoni <sarahmazoni94@gmail.com>
2026-09-12 22:49:12 -0300

    feat: rota da politica de privacidade

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
 	path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
 	path('password-reset-confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
 	path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
-
+	path('politica-privacidade/', views.politica_privacidade, name='politica_privacidade'),
 ]
